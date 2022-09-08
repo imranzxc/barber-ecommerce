1b59bec5fdec43e06147ac2ae36b0dfe0ac95198
Imran <imran.bursagov@gmail.com>
2022-09-08 17:53:06 +0300

    changes

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_URL = `${process.env.NEXT_PUBLIC_APP_API_URL}/api`;
-
+// todo
 export const getAuthUrl = (s: string) => `/auth${s}`;
 export const getMongo = (s: string) => `/connect${s}`;
